2fdd58617ebc7fa31d884674c6ed2311925d95e6
GET Academy <terje@getacademy.no>
2022-01-27 11:06:00 +0100

    Create Program3.cs

---
+++ GuessTheNumberInterface/DIP/Program3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumberInterface.DIP
+{
+    internal class Program3 : IProgram
+    {
+        public static void Run()
+        {
+            var program2 = new Program2();
+            program2.RunImpl();
+        }
+
+        public void RunImpl()
+        {
+            var logic = new Logic(this);
+            while (true)
+            {
+                logic.YesOrNo();
+            }
+        }
+
+        public void ShowAnswer(bool answer)
+        {
+            Console.WriteLine(answer);
+        }
+    }
+}
